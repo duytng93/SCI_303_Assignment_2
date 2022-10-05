d9245ef1a712a2b97eea803c4634b84223448cad
duytng93 <113152725+duytng93@users.noreply.github.com>
2022-10-05 07:36:58 -0500

    Update Main.cpp

--- Assignment 2_Question 2/Assignment 2_Question 2/Main.cpp
+++ Assignment 2_Question 2/Assignment 2_Question 2/Main.cpp
@@ -7,15 +7,15 @@
 using namespace std;
 int main() {
 
-	Employee* employee1;
+	Employee* employee1;			//create employees using poiners of base class for polymorphism	
 	Employee* employee2;
 	Employee* employee3;
 	Employee* employee4;
-	employee1 = new Professional("James Brown", 4000);
+	employee1 = new Professional("James Brown", 4000);		// pointers point to objects in derived classes
 	employee2 = new Nonprofessional("Oliver William", 20, 33);
 	employee3 = new Nonprofessional("Noah Smith", 18, 25);
 	employee4 = new Nonprofessional("Lucas Garcia", 16, 18);
-	employee1->displayInfo();
+	employee1->displayInfo();					//display employees information
 	cout << endl << "=======================" << endl<<endl;
 	employee2->displayInfo();
 	cout << endl << "=======================" << endl << endl;

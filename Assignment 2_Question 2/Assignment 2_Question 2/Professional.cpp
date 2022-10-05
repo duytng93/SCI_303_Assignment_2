57c29bdf5120d62e532fa2e4f9257ab4ca6cdfd2
duytng93 <113152725+duytng93@users.noreply.github.com>
2022-10-05 07:43:20 -0500

    Update Professional.cpp

--- Assignment 2_Question 2/Assignment 2_Question 2/Professional.cpp
+++ Assignment 2_Question 2/Assignment 2_Question 2/Professional.cpp
@@ -7,19 +7,19 @@ using namespace std;
 Professional::Professional(const std::string name, double salary)
 :Employee(name), monthlySalary(salary){};
 
-double Professional::weeklySalary() const
+double Professional::weeklySalary() const			// calculate weekly salary = monthly salary / 4
 {
 	return monthlySalary / 4;
 };
-double Professional::healthCareContribution() const
+double Professional::healthCareContribution() const		// calculate health care contribution = 5% of salary
 {
 	return (monthlySalary / 4) * 0.05;
 };
-double Professional::vacationLength() const
+double Professional::vacationLength() const			// vacation length is fixed for professional employees
 {
 	return vacationDays;
 };
-void Professional::displayInfo() const
+void Professional::displayInfo() const				// display employees information
 {
 	cout << "Employee name: " << fullName << endl;
 	cout << "Employee type: Professional" << endl;

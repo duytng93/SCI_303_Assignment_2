e3c1ea8425262328320aa1e1f3f96f09a1eed23d
duytng93 <113152725+duytng93@users.noreply.github.com>
2022-10-05 07:40:40 -0500

    Update Nonprofessional.cpp

--- Assignment 2_Question 2/Assignment 2_Question 2/Nonprofessional.cpp
+++ Assignment 2_Question 2/Assignment 2_Question 2/Nonprofessional.cpp
@@ -8,13 +8,13 @@ using namespace std;
 Nonprofessional::Nonprofessional(const std::string name, double rate, int hours)
 	:Employee(name), payRate(rate), weeklyHour(hours) {};
 
-double Nonprofessional::weeklySalary() const {
+double Nonprofessional::weeklySalary() const {				// calculate weekly salary = rate per hour * weekly work hours
 	return payRate * weeklyHour;
 };
-double Nonprofessional::healthCareContribution() const {
+double Nonprofessional::healthCareContribution() const {		// calculate health care contribution = 5% of salary
 	return payRate * weeklyHour * 0.05;
 };
-double Nonprofessional::vacationLength() const{
+double Nonprofessional::vacationLength() const{				//calculate vacation length depends on working hours per week
 	if (weeklyHour >= 30)
 		return 10;
 	else if (weeklyHour >= 20)
@@ -22,7 +22,7 @@ double Nonprofessional::vacationLength() const{
 	else
 		return 0;
 };
-void Nonprofessional::displayInfo() const {
+void Nonprofessional::displayInfo() const {				// display employees information
 	cout << "Employee name: " << fullName << endl;
 	cout << "Employee type: Non-Professional" << endl;
 	cout << "Weekly Salary: $" << weeklySalary() << endl;

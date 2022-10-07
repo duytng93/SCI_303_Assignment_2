99e368994961b7390e371a90d6719aa8d6919b21
duytng93 <113152725+duytng93@users.noreply.github.com>
2022-10-07 07:40:20 -0500

    Add files via upload

---
+++ Assignment 2_Question 2/Assignment 2_Question 2/Employee.h
@@ -0,0 +1,18 @@
+#ifndef EMPLOYEE_H_
+#define EMPLOYEE_H_
+#include <string>
+class Employee {
+private:
+	std::string fullName;
+public:
+	Employee(const std::string name)
+		:fullName(name) {};
+	std::string getName() {
+		return fullName;
+	};
+	virtual double weeklySalary() const = 0;				//using virtual key word for polymorphism
+	virtual double healthCareContribution() const = 0;
+	virtual double vacationLength() const = 0;
+	virtual void displayInfo() = 0;
+};
+#endif
